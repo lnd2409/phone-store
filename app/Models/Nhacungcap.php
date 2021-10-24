d479824933ef8b439a59a4dc79ff1d4be3a207df
Ngoc Duc <lnd240998@gmail.com>
2021-10-24 22:12:06 +0700

    hot fix

--- app/Models/Nhacungcap.php
+++ app/Models/Nhacungcap.php
@@ -1,50 +1,61 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Nhacungcap
- * 
- * @property int $ncc_id
+ * @property integer $ncc_id
  * @property string $ncc_ten
  * @property string $ncc_sdt
  * @property string $ncc_email
  * @property string $ncc_diachi
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * 
- * @property Collection|Khohang[] $khohangs
- * @property Collection|Sanpham[] $sanphams
- *
- * @package App\Models
+ * @property string $created_at
+ * @property string $updated_at
+ * @property Khohang[] $khohangs
+ * @property Sanpham[] $sanphams
  */
-class Nhacungcap extends Model
+class NhaCungCap extends Model
 {
-	protected $table = 'nhacungcap';
-	protected $primaryKey = 'ncc_id';
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'nhacungcap';
 
-	protected $fillable = [
-		'ncc_ten',
-		'ncc_sdt',
-		'ncc_email',
-		'ncc_diachi'
-	];
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ncc_id';
 
-	public function khohangs()
-	{
-		return $this->hasMany(Khohang::class, 'ncc_id');
-	}
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'integer';
 
-	public function sanphams()
-	{
-		return $this->hasMany(Sanpham::class, 'ncc_id');
-	}
+    /**
+     * @var array
+     */
+    protected $fillable = ['ncc_ten', 'ncc_sdt', 'ncc_email', 'ncc_diachi', 'created_at', 'updated_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function khohangs()
+    {
+        return $this->hasMany('App\Models\Khohang', 'ncc_id', 'ncc_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sanphams()
+    {
+        return $this->hasMany('App\Models\Sanpham', 'ncc_id', 'ncc_id');
+    }
 }
