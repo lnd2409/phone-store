65f435e666e13f915070cdf0bc43c2307ff915c3
Ngoc Duc <lnd240998@gmail.com>
2021-11-21 19:50:18 +0700

    add fix

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -8,6 +8,9 @@
         <div class="container overflow-hidden">
             <div class="js-slick-carousel u-slick"
                 data-pagi-classes="text-center position-absolute right-0 bottom-0 left-0 u-slick__pagination u-slick__pagination--long justify-content-center mb-3 mb-md-4">
+                @if (count($tinTuc) > 0)
+
+                @endif
                 @foreach ($tinTuc as $item)
                 <div class="js-slide">
                     <div class="row pt-7 py-md-0">
@@ -38,63 +41,6 @@
                     </div>
                 </div>
                 @endforeach
-{{--
-                <div class="js-slide">
-                    <div class="row pt-7 py-md-0">
-                        <div class="d-none d-wd-block offset-1"></div>
-                        <div class="col-xl col col-md-6 mt-md-8 mt-lg-10">
-                            <div class="ml-xl-4">
-                                <h6 class="font-size-15 font-weight-bold mb-2 text-cyan"
-                                    data-scs-animation-in="fadeInUp">
-                                    SHOP TO GET WHAT YOU LOVE
-                                </h6>
-                                <h1 class="font-size-46 text-lh-50 font-weight-light mb-8"
-                                    data-scs-animation-in="fadeInUp"
-                                    data-scs-animation-delay="200">
-                                    TIMEPIECES THAT MAKE A STATEMENT UP TO <stong class="font-weight-bold">40% OFF</stong>
-                                </h1>
-                                <a href="../shop/single-product-fullwidth.html" class="btn btn-primary transition-3d-hover rounded-lg font-weight-normal py-2 px-md-7 px-3 font-size-16"
-                                    data-scs-animation-in="fadeInUp"
-                                    data-scs-animation-delay="300">
-                                    Start Buying
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-7 col-6 flex-horizontal-center ml-auto ml-md-0"
-                            data-scs-animation-in="fadeInRight"
-                            data-scs-animation-delay="500">
-                            <img class="img-fluid ml-auto mr-10 mr-wd-auto h-100" src="{{ asset('template/client') }}/assets/img/416X420/img1.png" alt="Image Description">
-                        </div>
-                    </div>
-                </div>
-                <div class="js-slide">
-                    <div class="row pt-7 py-md-0">
-                        <div class="d-none d-wd-block offset-1"></div>
-                        <div class="col-xl col col-md-6 mt-md-8 mt-lg-10">
-                            <div class="ml-xl-4">
-                                <h6 class="font-size-15 font-weight-bold mb-2 text-cyan"
-                                    data-scs-animation-in="fadeInUp">
-                                    SHOP TO GET WHAT YOU LOVE
-                                </h6>
-                                <h1 class="font-size-46 text-lh-50 font-weight-light mb-8"
-                                    data-scs-animation-in="fadeInUp"
-                                    data-scs-animation-delay="200">
-                                    TIMEPIECES THAT MAKE A STATEMENT UP TO <stong class="font-weight-bold">40% OFF</stong>
-                                </h1>
-                                <a href="../shop/single-product-fullwidth.html" class="btn btn-primary transition-3d-hover rounded-lg font-weight-normal py-2 px-md-7 px-3 font-size-16"
-                                    data-scs-animation-in="fadeInUp"
-                                    data-scs-animation-delay="300">
-                                    Start Buying
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-xl-7 col-6 flex-horizontal-center ml-auto ml-md-0"
-                            data-scs-animation-in="fadeInRight"
-                            data-scs-animation-delay="500">
-                            <img class="img-fluid ml-auto mr-10 mr-wd-auto h-100" src="{{ asset('template/client') }}/assets/img/416X420/img3.png" alt="Image Description">
-                        </div>
-                    </div>
-                </div> --}}
             </div>
         </div>
     </div>
